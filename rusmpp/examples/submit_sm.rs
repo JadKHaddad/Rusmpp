f1fdf59a18126aa69603e843e39080aed365e50b
JadKHaddad <jadkhaddad@gmail.com>
2023-12-25 22:12:15 +0100

    example added

---
+++ rusmpp/examples/submit_sm.rs
@@ -0,0 +1,245 @@
+use std::{str::FromStr, sync::Arc};
+
+use rusmpp::{
+    io::{read::AsyncIoRead, write::AsyncIoWrite},
+    pdus::{
+        body::{
+            bodies::{bind::Bind, query_sm::QuerySm, sm::Sm, submit_sm::SubmitSm},
+            pdu_body::PduBody,
+        },
+        pdu::Pdu,
+        tlvs::{
+            tlv::MessageSubmissionRequestTLV,
+            tlv_value::{MessageSubmissionRequestTLVValue, TLVValue},
+            tlv_values::alert_on_msg_delivery::AlertOnMsgDelivery,
+        },
+        types::{
+            command_id::{CommandId, NoBodyCommandId},
+            command_status::CommandStatus,
+            data_coding::DataCoding,
+            esm_class::EsmClass,
+            interface_version::InterfaceVersion,
+            npi::Npi,
+            priority_flag::PriorityFlag,
+            registered_delivery::{
+                IntermediateNotification, MCDeliveryReceipt, RegisteredDelivery,
+                SmeOriginatedAcknowledgement,
+            },
+            replace_if_present_flag::ReplaceIfPresentFlag,
+            sequence_number::SequenceNumber,
+            service_type::{GenericServiceType, ServiceType},
+            ton::Ton,
+        },
+    },
+    types::{
+        c_octet_string::COctetString, empty_or_full_c_octet_string::EmptyOrFullCOctetString,
+        greater_than_u8::GreaterThanU8, no_fixed_size_octet_string::NoFixedSizeOctetString,
+        octet_string::OctetString,
+    },
+};
+use tokio::{
+    io::BufReader,
+    net::TcpStream,
+    sync::{mpsc, Notify},
+};
+
+const BIND_TRANSCEIVER_SEQUENCE_NUMBER: u32 = 1;
+const SUBMIT_SM_SEQUENCE_NUMBER: u32 = 2;
+const QUERY_SM_SEQUENCE_NUMBER: u32 = 3;
+const UNBIND_SEQUENCE_NUMBER: u32 = 4;
+
+#[tokio::main]
+async fn main() {
+    let stream = TcpStream::connect("34.242.18.250:2775")
+        .await
+        .expect("Failed to connect");
+
+    let (read, mut write) = stream.into_split();
+    let notify = Arc::new(Notify::new());
+    let notify_clone = notify.clone();
+    let (query_sm_tx, mut query_sm_rx) = mpsc::channel::<COctetString<1, 65>>(1);
+    let (is_delivered_tx, mut is_delivered_rx) = mpsc::channel::<COctetString<1, 65>>(1);
+
+    tokio::spawn(async move {
+        let bind_transceiver_pdu = Pdu::new(
+            CommandStatus::EsmeRok,
+            SequenceNumber::new(BIND_TRANSCEIVER_SEQUENCE_NUMBER),
+            PduBody::BindTransceiver(Bind {
+                system_id: COctetString::from_str("SMPP3TEST").unwrap(),
+                password: COctetString::from_str("secret08").unwrap(),
+                system_type: COctetString::from_str("SUBMIT1").unwrap(),
+                interface_version: InterfaceVersion::Smpp5_0,
+                addr_ton: Ton::Unknown,
+                addr_npi: Npi::Unknown,
+                address_range: COctetString::empty(),
+            }),
+        )
+        .unwrap();
+
+        // Do the bind
+        bind_transceiver_pdu
+            .async_io_write(&mut write)
+            .await
+            .expect("Failed to write pdu bytes");
+
+        // Wait until we are bound
+        notify.notified().await;
+        println!("We are bound!");
+        println!();
+
+        let submit_sm = SubmitSm::new(
+            Sm::new(ServiceType::new(GenericServiceType::default()).unwrap(),
+            Ton::Unknown,
+            Npi::Unknown,
+            COctetString::from_str("SomeSource").unwrap(),
+            Ton::Unknown,
+            Npi::Unknown,
+            COctetString::from_str("SomeDest").unwrap(),
+            EsmClass::default(),
+            0,
+            PriorityFlag::default(),
+            EmptyOrFullCOctetString::from_str("").unwrap(),
+            EmptyOrFullCOctetString::from_str("").unwrap(),
+            // Use default values to "not" get a delivery receipt
+            RegisteredDelivery::new(
+                MCDeliveryReceipt::McDeliveryReceiptRequestedWhereFinalDeliveryOutcomeIsSuccessOrFailure,
+                SmeOriginatedAcknowledgement::NoReceiptSmeAcknowledgementRequested,
+                IntermediateNotification::IntermediateNotificationRequested,
+                0,
+            ),
+            ReplaceIfPresentFlag::default(),
+            DataCoding::default(),
+            GreaterThanU8::new(1).unwrap(),
+            OctetString::from_str("Hi, I am a short message. I will be overridden :(").unwrap()),
+            // Optional TLVs
+            vec![
+                MessageSubmissionRequestTLV::new(MessageSubmissionRequestTLVValue::MessagePayload(
+                    NoFixedSizeOctetString::from_str(
+                        "Hi, I am a very long message that will override the short message :D",
+                    )
+                    .unwrap(),
+                )),
+                MessageSubmissionRequestTLV::new(
+                    MessageSubmissionRequestTLVValue::AlertOnMsgDelivery(
+                        AlertOnMsgDelivery::UseMobileDefaultAlert,
+                    ),
+                ),
+                MessageSubmissionRequestTLV::new(MessageSubmissionRequestTLVValue::CallbackNum(
+                    OctetString::from_str("0000").unwrap(),
+                )),
+            ],
+        );
+
+        let source_addr_ton = submit_sm.sm().source_addr_ton();
+        let source_addr_npi = submit_sm.sm().source_addr_npi();
+        let source_addr = submit_sm.sm().source_addr().clone();
+
+        // Submit the message
+        let submit_sm_pdu = Pdu::new(
+            CommandStatus::EsmeRok,
+            SequenceNumber::new(SUBMIT_SM_SEQUENCE_NUMBER),
+            PduBody::SubmitSm(submit_sm),
+        )
+        .unwrap();
+
+        submit_sm_pdu
+            .async_io_write(&mut write)
+            .await
+            .expect("Failed to write pdu bytes");
+
+        let Some(message_id) = query_sm_rx.recv().await else {
+            panic!("What happened to our channel!?");
+        };
+
+        println!("We did submit_sm!");
+        println!("Message id: {}", message_id.to_string());
+        println!();
+
+        // Query the status of the message, if you want :v
+        let query_sm_pdu = Pdu::new(
+            CommandStatus::EsmeRok,
+            SequenceNumber::new(QUERY_SM_SEQUENCE_NUMBER),
+            PduBody::QuerySm(QuerySm {
+                message_id,
+                source_addr_ton,
+                source_addr_npi,
+                source_addr,
+            }),
+        )
+        .unwrap();
+
+        query_sm_pdu
+            .async_io_write(&mut write)
+            .await
+            .expect("Failed to write pdu bytes");
+
+        // Ok now wait until the message is delivered
+        let Some(message_id) = is_delivered_rx.recv().await else {
+            panic!("What happened to our channel!?");
+        };
+
+        println!(
+            "Well, Message with id: {} is delivered!",
+            message_id.to_string()
+        );
+        println!();
+
+        // Ok now we are done, let's unbind
+        let unbind_pdu = Pdu::new_without_body(
+            NoBodyCommandId::Unbind,
+            CommandStatus::EsmeRok,
+            SequenceNumber::new(4),
+        )
+        .unwrap();
+
+        unbind_pdu
+            .async_io_write(&mut write)
+            .await
+            .expect("Failed to write pdu bytes");
+
+        // Wait until we are unbound
+        notify.notified().await;
+        println!("We are unbound!");
+    });
+
+    let mut buf_reader = BufReader::new(read);
+
+    while let Ok(pdu) = Pdu::async_io_read(&mut buf_reader).await {
+        println!("{:?}", pdu);
+        println!();
+
+        if let CommandId::UnbindResp = pdu.command_id() {
+            if pdu.sequence_number().value == UNBIND_SEQUENCE_NUMBER {
+                notify_clone.notify_one();
+            }
+        }
+
+        match pdu.body() {
+            Some(PduBody::BindTransceiverResp(_)) => {
+                if pdu.sequence_number().value == BIND_TRANSCEIVER_SEQUENCE_NUMBER {
+                    notify_clone.notify_one();
+                }
+            }
+            Some(PduBody::SubmitSmResp(submit_sm_resp)) => {
+                if pdu.sequence_number().value == SUBMIT_SM_SEQUENCE_NUMBER {
+                    let message_id = submit_sm_resp.message_id().clone();
+                    if query_sm_tx.send(message_id).await.is_err() {
+                        panic!("What happened to our channel!?");
+                    }
+                }
+            }
+            Some(PduBody::DeliverSm(deliver_sm)) => {
+                for tlv in deliver_sm.tlvs().iter() {
+                    if let Some(TLVValue::ReceiptedMessageId(message_id)) = tlv.value() {
+                        if is_delivered_tx.send(message_id.clone()).await.is_err() {
+                            panic!("What happened to our channel!?");
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    println!("Connection closed");
+}
--- rusmpp/src/lib.rs
+++ rusmpp/src/lib.rs
@@ -9,43 +9,20 @@ mod tests {
 
     use tokio::{
         io::{AsyncWriteExt, BufReader},
-        net::{TcpListener, TcpStream},
+        net::TcpStream,
     };
 
     use crate::{
         io::{read::AsyncIoRead, write::AsyncIoWrite},
         pdus::{
-            body::{
-                bodies::{bind::Bind, query_sm::QuerySm, sm::Sm, submit_sm::SubmitSm},
-                pdu_body::PduBody,
-            },
+            body::{bodies::bind::Bind, pdu_body::PduBody},
             pdu::Pdu,
-            tlvs::{
-                tlv::MessageSubmissionRequestTLV, tlv_value::MessageSubmissionRequestTLVValue,
-                tlv_values::alert_on_msg_delivery::AlertOnMsgDelivery,
-            },
             types::{
-                command_status::CommandStatus,
-                data_coding::DataCoding,
-                esm_class::EsmClass,
-                interface_version::InterfaceVersion,
-                npi::Npi,
-                priority_flag::PriorityFlag,
-                registered_delivery::{
-                    IntermediateNotification, MCDeliveryReceipt, RegisteredDelivery,
-                    SmeOriginatedAcknowledgement,
-                },
-                replace_if_present_flag::ReplaceIfPresentFlag,
-                sequence_number::SequenceNumber,
-                service_type::{GenericServiceType, ServiceType},
-                ton::Ton,
+                command_status::CommandStatus, interface_version::InterfaceVersion, npi::Npi,
+                sequence_number::SequenceNumber, ton::Ton,
             },
         },
-        types::{
-            c_octet_string::COctetString, empty_or_full_c_octet_string::EmptyOrFullCOctetString,
-            greater_than_u8::GreaterThanU8, no_fixed_size_octet_string::NoFixedSizeOctetString,
-            octet_string::OctetString,
-        },
+        types::c_octet_string::COctetString,
     };
 
     fn create_default_bind() -> Bind {
@@ -58,49 +35,6 @@ mod tests {
             addr_npi: Npi::Unknown,
             address_range: COctetString::empty(),
         }
-    }
-
-    fn create_default_submit_sm() -> SubmitSm {
-        SubmitSm::new(
-            Sm::new(ServiceType::new(GenericServiceType::default()).unwrap(),
-            Ton::Unknown,
-            Npi::Unknown,
-            COctetString::from_str("SomeSource").unwrap(),
-            Ton::Unknown,
-            Npi::Unknown,
-            COctetString::from_str("SomeDest").unwrap(),
-            EsmClass::default(),
-            0,
-            PriorityFlag::default(),
-            EmptyOrFullCOctetString::from_str("").unwrap(),
-            EmptyOrFullCOctetString::from_str("").unwrap(),
-            RegisteredDelivery::new(
-                MCDeliveryReceipt::McDeliveryReceiptRequestedWhereFinalDeliveryOutcomeIsSuccessOrFailure,
-                SmeOriginatedAcknowledgement::NoReceiptSmeAcknowledgementRequested,
-                IntermediateNotification::IntermediateNotificationRequested,
-                0,
-            ),
-            ReplaceIfPresentFlag::default(),
-            DataCoding::default(),
-            GreaterThanU8::new(1).unwrap(),
-            OctetString::from_str("Hi, I am a short message. I will be overridden :(").unwrap()),
-            vec![
-                MessageSubmissionRequestTLV::new(MessageSubmissionRequestTLVValue::MessagePayload(
-                    NoFixedSizeOctetString::from_str(
-                        "Hi, I am a very long message that will override the short message :D",
-                    )
-                    .unwrap(),
-                )),
-                MessageSubmissionRequestTLV::new(
-                    MessageSubmissionRequestTLVValue::AlertOnMsgDelivery(
-                        AlertOnMsgDelivery::UseMobileDefaultAlert,
-                    ),
-                ),
-                MessageSubmissionRequestTLV::new(MessageSubmissionRequestTLVValue::CallbackNum(
-                    OctetString::from_str("0000").unwrap(),
-                )),
-            ],
-        )
     }
 
     async fn write_pdu<'a, T>(pdu: &'a Pdu, stream: &'a mut T)
@@ -193,211 +127,5 @@ mod tests {
         let pdus = connect_send_recv(vec![pdu]).await;
         let body = pdus[0].clone().into_body().expect("Expected pdu body");
         assert!(matches!(body, PduBody::BindTransceiverResp(_)));
-    }
-
-    #[tokio::test]
-    async fn bind_and_submit_sm_and_query_sm_and_recv_delivery() {
-        let stream = TcpStream::connect("34.242.18.250:2775")
-            .await
-            .expect("Failed to connect");
-
-        let (read, mut write) = stream.into_split();
-        let mut buf_reader = BufReader::new(read);
-
-        let bind_transceiver_pdu = Pdu::new(
-            CommandStatus::EsmeRok,
-            SequenceNumber::new(1),
-            PduBody::BindTransceiver(create_default_bind()),
-        )
-        .unwrap();
-        write_pdu(&bind_transceiver_pdu, &mut write).await;
-
-        match Pdu::async_io_read(&mut buf_reader).await {
-            Ok(pdu) => {
-                println!("BindTransceiverResp pdu: {:#?}", pdu);
-
-                let body = pdu.into_body().expect("Expected pdu body");
-                assert!(matches!(body, PduBody::BindTransceiverResp(_)));
-            }
-            Err(e) => {
-                panic!("error parsing pdu: {}", e)
-            }
-        }
-
-        let submit_sm_body = create_default_submit_sm();
-
-        let source_addr_ton = submit_sm_body.sm().source_addr_ton();
-        let source_addr_npi = submit_sm_body.sm().source_addr_npi();
-        let source_addr = submit_sm_body.sm().source_addr().clone();
-
-        let submit_sm_pdu = Pdu::new(
-            CommandStatus::EsmeRok,
-            SequenceNumber::new(2),
-            PduBody::SubmitSm(submit_sm_body),
-        )
-        .unwrap();
-        write_pdu(&submit_sm_pdu, &mut write).await;
-
-        match Pdu::async_io_read(&mut buf_reader).await {
-            Ok(pdu) => {
-                println!("pdu: {:#?}", pdu);
-
-                let body = pdu.into_body().expect("Expected pdu body");
-
-                match body {
-                    PduBody::SubmitSmResp(submit_sm_resp) => {
-                        let message_id = submit_sm_resp.message_id().clone();
-                        let query_sm_pdu = Pdu::new(
-                            CommandStatus::EsmeRok,
-                            SequenceNumber::new(3),
-                            PduBody::QuerySm(QuerySm {
-                                message_id,
-                                source_addr_ton,
-                                source_addr_npi,
-                                source_addr,
-                            }),
-                        )
-                        .unwrap();
-                        write_pdu(&query_sm_pdu, &mut write).await;
-
-                        match Pdu::async_io_read(&mut buf_reader).await {
-                            Ok(pdu) => {
-                                println!("QuerySmResp pdu: {:#?}", pdu);
-
-                                let body = pdu.into_body().expect("Expected pdu body");
-                                match body {
-                                    PduBody::QuerySmResp(_) => {}
-                                    PduBody::DeliverSm(_) => {}
-                                    _ => {
-                                        panic!("Unexpected pdu body");
-                                    }
-                                }
-                            }
-                            Err(e) => {
-                                panic!("error parsing pdu: {}", e)
-                            }
-                        };
-                    }
-                    PduBody::DeliverSm(_) => {}
-                    _ => {
-                        panic!("Unexpected pdu body");
-                    }
-                }
-            }
-            Err(e) => {
-                panic!("error parsing pdu: {}", e)
-            }
-        }
-    }
-
-    #[tokio::test]
-    #[ignore]
-    async fn send_three() {
-        let mut stream = TcpStream::connect("34.242.18.250:2775")
-            .await
-            .expect("Failed to connect");
-
-        Pdu::new(
-            CommandStatus::EsmeRok,
-            SequenceNumber::new(1),
-            PduBody::BindTransceiver(create_default_bind()),
-        )
-        .unwrap()
-        .async_io_write(&mut stream)
-        .await
-        .expect("Failed to write pdu bytes to steam");
-
-        Pdu::new(
-            CommandStatus::EsmeRok,
-            SequenceNumber::new(2),
-            PduBody::BindTransmitter(create_default_bind()),
-        )
-        .unwrap()
-        .async_io_write(&mut stream)
-        .await
-        .expect("Failed to write pdu bytes to steam");
-
-        Pdu::new(
-            CommandStatus::EsmeRok,
-            SequenceNumber::new(3),
-            PduBody::BindReceiver(create_default_bind()),
-        )
-        .unwrap()
-        .async_io_write(&mut stream)
-        .await
-        .expect("Failed to write pdu bytes to steam");
-
-        let mut buf_reader = BufReader::new(stream);
-        for _ in 0..3 {
-            match Pdu::async_io_read(&mut buf_reader).await {
-                Ok(pdu) => {
-                    println!("pdu: {:#?}", pdu);
-                }
-                Err(e) => {
-                    eprintln!("error parsing pdu: {}", e)
-                }
-            }
-        }
-    }
-
-    #[tokio::test]
-    async fn invalid_pdu_with_generic_nack() {
-        // TODO
-    }
-
-    #[tokio::test]
-    #[ignore]
-    async fn send_slow() {
-        tokio::spawn(async move {
-            let listener = TcpListener::bind("127.0.0.1:8080")
-                .await
-                .expect("Failed to bind");
-
-            println!("Listening on: {}", listener.local_addr().unwrap());
-
-            loop {
-                let (stream, _) = listener.accept().await.expect("Failed to accept");
-                tokio::spawn(async move {
-                    let mut buf_reader = BufReader::new(stream);
-
-                    match Pdu::async_io_read(&mut buf_reader).await {
-                        Ok(pdu) => {
-                            println!("Got pdu: {:#?}", pdu);
-                        }
-                        Err(e) => {
-                            eprintln!("error parsing pdu: {}", e)
-                        }
-                    }
-                });
-            }
-        });
-
-        let pdu = Pdu::new(
-            CommandStatus::EsmeRok,
-            SequenceNumber::new(1),
-            PduBody::BindTransceiver(create_default_bind()),
-        )
-        .unwrap();
-
-        let mut pdu_bytes = Vec::new();
-        pdu.async_io_write(&mut pdu_bytes)
-            .await
-            .expect("Failed to write pdu bytes to vec");
-
-        let mut stream = TcpStream::connect("127.0.0.1:8080")
-            .await
-            .expect("Failed to connect");
-
-        for byte in pdu_bytes {
-            println!("Sending byte: {:#02x}", byte);
-            stream
-                .write_u8(byte)
-                .await
-                .expect("Failed to write pdu bytes to steam");
-            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-        }
-
-        println!("Done sending pdu bytes");
-        tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
     }
 }

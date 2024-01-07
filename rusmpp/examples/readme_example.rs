05d7891bfe7355e92646915753e86da90545daf7
JadKHaddad <jadkhaddad@gmail.com>
2024-01-07 17:58:40 +0100

    updated readme example

--- rusmpp/examples/readme_example.rs
+++ rusmpp/examples/readme_example.rs
@@ -1,26 +1,5 @@
-# Rusmpp
+#![allow(unused_variables)]
 
-![Tests](https://github.com/JadKHaddad/Rusmpp/actions/workflows/tests.yml/badge.svg?branch=main)
-
-Low level SMPP library in pure rust. This is not a Client/Server implementation, but a library to build one. Baisc operations like `bind`, `unbind`, `submit_sm` and `deliver_sm` should not be difficult to implement. See examples for more details. Basic knowledge of SMPP protocol is required.
-
-## Semantics
-
-- This library is designed to be as close to the SMPP protocol as possible, reads/writes pdu-bytes from/into rust structs and provides a higer level API to the user. This means that the library does not do any validation of the data, and it is up to the user to ensure that the data is semantically valid.
-- Some semantics are type checked, for example the minimum and maximum length of a field is guaranteed by the type of the field. For example, the `system_id` field in the `Bind` operation is of type `COctetString<1, 16>` which means that the minimum length of the field is 1 (empty) and the maximum length is 16.
-- Other semantics are checked at runtime, for example, the `command_length` field in the header is automatically calculated and set by the library, and the user does not need to set it manually. Similarly, the `command_id` field is automatically set when the user provides a `Body` enum to the `Pdu::new` function. The `command_status` and `sequence_number` fields are also validated against `GeniricNack` responses.
-
-## Supported Operations
-
-All operations are supported.
-
-## Supported TLVs
-
-All TLVs are supported.
-
-## Example
-
-```rust
 use rusmpp::{
     pdus::body::bodies::bind::{Bind, BindBuilder},
     prelude::*,
@@ -79,10 +58,3 @@ async fn main() {
         }
     }
 }
-```
-
-See the [examples](https://github.com/JadKHaddad/Rusmpp/tree/main/rusmpp/examples) directory for more details.
-
-## Releases
-
-`vec![]`

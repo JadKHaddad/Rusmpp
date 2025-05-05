3a9be7fd13c35e3c01daff703ea39bdafeeb240d
Matthieu Casanova <chocolat.mou@gmail.com>
2025-05-05 21:39:29 +0200

    add doc to run the example

--- examples/client_server.rs
+++ examples/client_server.rs
@@ -1,5 +1,10 @@
-/// Demonstrate a SMPP server and a client sending an EnquireLink
-/// This example requires `tokio-codec` feature
+//! Demonstrate a SMPP server and a client sending an EnquireLink
+//! Run with
+//!
+//! ```not_rust
+//! cargo run --example client_server --features="tokio-codec"
+//! ```
+//!
 
 use futures::{SinkExt, StreamExt};
 use rusmpp::{
@@ -20,7 +25,11 @@ async fn launch_server(server_stream: DuplexStream) -> Result<(), Box<dyn std::e
         while let Some(Ok(command)) = framed.next().await {
             if let CommandId::EnquireLink = command.command_id() {
                 println!("Server: EnquireLink received");
-                let response = Command::new(CommandStatus::EsmeRok, command.sequence_number, Pdu::EnquireLinkResp);
+                let response = Command::new(
+                    CommandStatus::EsmeRok,
+                    command.sequence_number,
+                    Pdu::EnquireLinkResp,
+                );
                 framed.send(&response).await.unwrap();
                 println!("Server: EnquireLink response sent");
                 break;

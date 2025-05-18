39bae16ec67f8663ef80c2aeceb5354dd849b180
Jad K. Haddad <jadkhaddad@gmail.com>
2025-05-18 10:58:56 +0200

    docs(CommandCodec): removed usage section
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- src/codec/command_codec.rs
+++ src/codec/command_codec.rs
@@ -1,46 +1,4 @@
 /// Codec for encoding and decoding `SMPP` PDUs.
-///
-/// # Usage
-/// ```rust
-/// use futures::{SinkExt, StreamExt};
-/// use rusmpp::{codec::CommandCodec, Command, CommandId, CommandStatus, Pdu};
-/// use tokio::io::DuplexStream;
-/// use tokio_util::codec::Framed;
-///
-/// async fn launch_server(server_stream: DuplexStream) -> Result<(), Box<dyn core::error::Error>> {
-///     tokio::spawn(async move {
-///         let mut framed = Framed::new(server_stream, CommandCodec::new());
-///
-///         while let Some(Ok(command)) = framed.next().await {
-///             if let CommandId::EnquireLink = command.id() {
-///                 let response = Command::new(CommandStatus::EsmeRok, command.sequence_number, Pdu::EnquireLinkResp);
-///                 framed.send(&response).await.unwrap();
-///                 break;
-///             }
-///         }
-///     });
-///     Ok(())
-/// }
-///
-/// #[tokio::main]
-/// async fn main() -> Result<(), Box<dyn core::error::Error>> {
-///     let (server_stream, client_stream) = tokio::io::duplex(4096);
-///     launch_server(server_stream).await?;
-///
-///     let mut framed = Framed::new(client_stream, CommandCodec::new());
-///
-///     let enquire_link_command = Command::new(CommandStatus::EsmeRok, 0, Pdu::EnquireLink);
-///     framed.send(&enquire_link_command).await?;
-///
-///     while let Some(Ok(command)) = framed.next().await {
-///         if let CommandId::EnquireLinkResp = command.id() {
-///             break;
-///         }
-///     }
-///
-///     Ok(())
-/// }
-/// ```
 #[derive(Debug)]
 pub struct CommandCodec {
     max_length: Option<usize>,

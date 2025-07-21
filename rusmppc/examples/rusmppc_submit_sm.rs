116a72b17db287e3f565413c38125af0e3bcc7b2
Jad K. Haddad <jadkhaddad@gmail.com>
2025-07-21 20:17:51 +0200

    fix(examples): fixed tracing target rusmppc_submit_sm in rusmppc/rusmppc_submit_sm.rs
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- rusmppc/examples/rusmppc_submit_sm.rs
+++ rusmppc/examples/rusmppc_submit_sm.rs
@@ -23,7 +23,7 @@ async fn main() -> Result<(), Box<dyn core::error::Error>> {
     // Rusmppc produces a lot of logs while managing the SMPP connection in the background.
     // You can filter them out by setting the `rusmppc` target to `off`.
     tracing_subscriber::fmt()
-        .with_env_filter("submit_sm=info,rusmpp=off,rusmppc=debug")
+        .with_env_filter("rusmppc_submit_sm=info,rusmpp=off,rusmppc=debug")
         .init();
 
     let (client, mut events) = ConnectionBuilder::new()

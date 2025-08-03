d53b151564a96782845f7d51e11580bc5645530c
Jad K. Haddad <jadkhaddad@gmail.com>
2025-08-03 09:56:22 +0200

    feat(rusmpps/config): moved Config to ConnectionConfig
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- rusmpps/src/config.rs
+++ rusmpps/src/config.rs
@@ -1,15 +1,1 @@
-use std::time::Duration;
 
-use crate::client::{Client, ConnectedClients};
-
-#[derive(Debug)]
-pub struct Config {
-    pub connected_clients: ConnectedClients,
-    pub clients: Vec<Client>,
-    pub enquire_link_interval: Duration,
-    pub response_timeout: Duration,
-    pub session_timeout: Duration,
-    pub bind_delay: Duration,
-    pub response_delay: Duration,
-    pub enquire_link_response_delay: Duration,
-}

acb6263bc099649dffe59fb9ac677bf41781aa1b
jha <jad.haddad@gselectronic.com>
2025-08-04 10:47:57 +0200

    feat(rusmppyc/examples): close the StreamWriter in the connected.py example
    Signed-off-by: jha <jad.haddad@gselectronic.com>

--- rusmppy/rusmppyc/python/examples/connected.py
+++ rusmppy/rusmppyc/python/examples/connected.py
@@ -1,18 +1,25 @@
-
 from rusmppyc import Events, Client
 
 import asyncio
+
 
 async def handle_events(events: Events):
     async for event in events:
         print(f"Received event: {event}")
 
-async def main():
-    # using async io connect to 127.0.0.1:2775
-    read, write = await asyncio.open_connection(
-        '127.0.0.1', 2775)
+    print("Event handling completed.")
 
-    client, events = await Client.connected(read, write, enquire_link_interval=5, enquire_link_response_timeout=2, response_timeout=2)
+
+async def main():
+    read, write = await asyncio.open_connection("127.0.0.1", 2775)
+
+    client, events = await Client.connected(
+        read,
+        write,
+        enquire_link_interval=5,
+        enquire_link_response_timeout=2,
+        response_timeout=2,
+    )
 
     asyncio.create_task(handle_events(events))
 
@@ -22,6 +29,18 @@ async def main():
     )
 
     print(f"Bind response: {response}")
+
+    await client.unbind()
+    await client.close()
+    await client.closed()
+
+    await asyncio.sleep(3)
+
+    # At this point the tcp connection is not closed
+    # Rust does NOT close the StreamWriter
+    write.close()
+    await write.wait_closed()
+
 
 if __name__ == "__main__":
     asyncio.run(main())

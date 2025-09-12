f45bfadd3e59b9a17fc006c423fcd4c3af0c8128
Jad K. Haddad <jadkhaddad@gmail.com>
2025-09-12 09:50:02 +0200

    refactor(rusmpp-macros): moved derive logic to derive module
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

---
+++ rusmpp-macros/src/derive.rs
@@ -0,0 +1,165 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{Data, DeriveInput, Ident, Lit, parse};
+
+pub fn derive(input: DeriveInput) -> syn::Result<TokenStream> {
+    match &input.data {
+        Data::Struct(_) => derive_struct(&input),
+        Data::Enum(_) => derive_enum(&input),
+        _ => panic!("Rusmpp can only be derived for enums or structs with named fields"),
+    }
+}
+
+fn derive_struct(input: &DeriveInput) -> syn::Result<TokenStream> {
+    let struct_attrs = StructAttributes::extract(input)?;
+
+    if let Some(repr) = struct_attrs.repr {
+        return Ok(repr.expand(&input.ident));
+    }
+
+    let expanded = quote! {};
+
+    Ok(expanded)
+}
+
+struct StructAttributes {
+    /// #[rusmpp(repr = "u8")]
+    repr: Option<Repr>,
+    /// #[rusmpp(skip_decode)]
+    skip_decode: bool,
+}
+
+impl StructAttributes {
+    fn extract(input: &DeriveInput) -> syn::Result<Self> {
+        let mut repr: Option<Repr> = None;
+        let mut skip_decode = false;
+
+        for attr in &input.attrs {
+            if !attr.path().is_ident("rusmpp") {
+                continue;
+            }
+
+            attr.parse_nested_meta(|meta| {
+                if meta.path.is_ident("repr") {
+                    let lit: Lit = meta.value()?.parse()?;
+
+                    match lit {
+                        Lit::Str(s) => match s.value().as_str() {
+                            "u8" => repr = Some(Repr::U8),
+                            other => {
+                                return Err(meta.error(format!(
+                                    "unsupported repr: {}, only `u8` allowed",
+                                    other
+                                )));
+                            }
+                        },
+                        _ => {
+                            return Err(meta.error("repr must be a string literal like \"u8\""));
+                        }
+                    }
+                } else if meta.path.is_ident("skip_decode") {
+                    skip_decode = true;
+                }
+
+                Ok(())
+            })?;
+        }
+
+        Ok(Self { repr, skip_decode })
+    }
+}
+
+struct FieldAttributes {
+    skip_decode: bool,
+    length: Option<Length>,
+    key: Option<Ident>,
+    count: Option<Ident>,
+}
+
+enum Length {
+    Unchecked,
+    Checked,
+    Ident(Ident),
+}
+
+fn derive_enum(input: &DeriveInput) -> syn::Result<TokenStream> {
+    let enum_attrs = EnumAttributes::extract(input)?;
+
+    Ok(enum_attrs.repr.expand(&input.ident))
+}
+
+struct EnumAttributes {
+    /// #[repr(u8)]
+    repr: Repr,
+}
+
+impl EnumAttributes {
+    fn extract(input: &DeriveInput) -> syn::Result<Self> {
+        let mut repr: Option<Repr> = None;
+
+        for attr in &input.attrs {
+            if attr.path().is_ident("repr") {
+                attr.parse_args_with(|input: parse::ParseStream| {
+                    let ident: Ident = input.parse()?;
+
+                    match ident.to_string().as_str() {
+                        "u8" => repr = Some(Repr::U8),
+                        other => {
+                            return Err(syn::Error::new_spanned(
+                                ident,
+                                format!("unsupported repr: {}, only `u8` allowed", other),
+                            ));
+                        }
+                    }
+
+                    Ok(())
+                })?;
+            }
+        }
+
+        let repr = repr.ok_or_else(|| {
+            syn::Error::new_spanned(
+                input,
+                "enums must have a #[repr(...)] attribute, e.g. #[repr(u8)]",
+            )
+        })?;
+
+        Ok(Self { repr })
+    }
+}
+
+enum Repr {
+    U8,
+}
+
+impl Repr {
+    fn to_ident(&self) -> Ident {
+        match self {
+            Repr::U8 => Ident::new("u8", proc_macro2::Span::call_site()),
+        }
+    }
+
+    fn expand(&self, name: &Ident) -> TokenStream {
+        let repr_ident = self.to_ident();
+
+        quote! {
+            impl ::rusmpp_core::encode::Length for #name {
+                fn length(&self) -> usize {
+                    #repr_ident::from(*self).length()
+                }
+            }
+
+            impl ::rusmpp_core::encode::Encode for #name {
+                fn encode(&self, dst: &mut [u8]) -> usize {
+                    #repr_ident::from(*self).encode(dst)
+                }
+            }
+
+            impl ::rusmpp_core::decode::Decode for #name {
+                fn decode(src: &[u8]) -> Result<(Self, usize), ::rusmpp_core::decode::DecodeError> {
+                    #repr_ident::decode(src).map(|(this, size)| (Self::from(this), size))
+                }
+            }
+        }
+    }
+}
--- rusmpp-macros/src/lib.rs
+++ rusmpp-macros/src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{Data, DeriveInput, Ident, Lit, parse, parse_macro_input};
+use syn::{DeriveInput, parse_macro_input};
+
+mod derive;
 
 // TODO: parts
 
@@ -18,11 +19,11 @@ use syn::{Data, DeriveInput, Ident, Lit, parse, parse_macro_input};
 /// ## Container attributes
 ///
 /// - `#[rusmpp(repr = "u8")]`: Use the `From<u8>`/`Into<u8>` representation for decoding.
-/// - `#[rusmpp(skip_decode)]`: Skip implementing the `Decode` trait.
+/// - `#[rusmpp(skip_decode)]`: Skip implementing the `Decode` trait if the struct is not annotated with a `repr`.
 ///
 /// ## Field attributes
 ///
-/// - `#[rusmpp(skip)]`: Skip decoding the field (requires a corresponding `new` constructor that does not take the skipped field as an argument).
+/// - `#[rusmpp(skip_decode)]`: Skip decoding the field (requires a corresponding `new` constructor that does not take the skipped field as an argument).
 /// - `#[rusmpp(length = "unchecked")]`: Decode without length checks.
 /// - `#[rusmpp(length = "checked")]`: Decode using `length_checked_decode`.
 /// - `#[rusmpp(length = ident)]`: Use the value of another field (`ident`) as the length for decoding.
@@ -37,133 +38,7 @@ use syn::{Data, DeriveInput, Ident, Lit, parse, parse_macro_input};
 pub fn rusmpp(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    let name = &input.ident;
-
-    match &input.data {
-        Data::Struct(_) => {
-            // extract #[rusmpp(repr = "u8")] if exists. default: no repr
-            let mut repr: Option<Repr> = None;
-
-            // extract #[rusmpp(skip_decode)] if exists. default: false
-            let mut skip_decode = false;
-
-            for attr in &input.attrs {
-                if !attr.path().is_ident("rusmpp") {
-                    continue;
-                }
-
-                let result = attr.parse_nested_meta(|meta| {
-                    if meta.path.is_ident("repr") {
-                        // #[rusmpp(repr = "u8")]
-                        let lit: Lit = meta.value()?.parse()?;
-
-                        match lit {
-                            Lit::Str(s) => match s.value().as_str() {
-                                "u8" => repr = Some(Repr::U8),
-                                other => {
-                                    return Err(meta.error(format!(
-                                        "unsupported repr: {}, only `u8` allowed",
-                                        other
-                                    )));
-                                }
-                            },
-                            _ => {
-                                return Err(meta.error("repr must be a string literal like \"u8\""));
-                            }
-                        }
-                    } else if meta.path.is_ident("skip_decode") {
-                        // #[rusmpp(skip_decode)]
-                        skip_decode = true;
-                    }
-
-                    Ok(())
-                });
-
-                if let Err(err) = result {
-                    err.to_compile_error();
-                }
-            }
-
-            if let Some(repr) = repr {
-                return repr.expand(name);
-            }
-
-            let expanded = quote! {};
-
-            TokenStream::from(expanded)
-        }
-
-        Data::Enum(_) => {
-            // extract #[repr(u8)] if exists. default: no repr
-            let mut repr: Option<Repr> = None;
-
-            for attr in &input.attrs {
-                if attr.path().is_ident("repr") {
-                    let result = attr.parse_args_with(|input: parse::ParseStream| {
-                        let ident: Ident = input.parse()?;
-
-                        match ident.to_string().as_str() {
-                            "u8" => repr = Some(Repr::U8),
-                            other => {
-                                return Err(syn::Error::new_spanned(
-                                    ident,
-                                    format!("unsupported repr: {}, only `u8` allowed", other),
-                                ));
-                            }
-                        }
-
-                        Ok(())
-                    });
-
-                    if let Err(err) = result {
-                        err.to_compile_error();
-                    }
-                }
-            }
-
-            let repr = repr.expect("enums must have a #[repr(...)] attribute");
-
-            repr.expand(name)
-        }
-
-        _ => panic!("Rusmpp can only be derived for enums or structs with named fields"),
-    }
-}
-
-enum Repr {
-    U8,
-}
-
-impl Repr {
-    fn to_ident(&self) -> Ident {
-        match self {
-            Repr::U8 => Ident::new("u8", proc_macro2::Span::call_site()),
-        }
-    }
-
-    fn expand(&self, name: &Ident) -> TokenStream {
-        let repr_ident = self.to_ident();
-
-        let expanded = quote! {
-            impl ::rusmpp_core::encode::Length for #name {
-                fn length(&self) -> usize {
-                    #repr_ident::from(*self).length()
-                }
-            }
-
-            impl ::rusmpp_core::encode::Encode for #name {
-                fn encode(&self, dst: &mut [u8]) -> usize {
-                    #repr_ident::from(*self).encode(dst)
-                }
-            }
-
-            impl ::rusmpp_core::decode::Decode for #name {
-                fn decode(src: &[u8]) -> Result<(Self, usize), ::rusmpp_core::decode::DecodeError> {
-                    #repr_ident::decode(src).map(|(this, size)| (Self::from(this), size))
-                }
-            }
-        };
-
-        TokenStream::from(expanded)
-    }
+    derive::derive(input)
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
 }

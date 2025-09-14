067018d7f29b6ca3339e302804aa68c0bb0b054a
fab <fabian.bernhart@gselectronic.com>
2025-09-14 14:20:46 +0200

    fix(rusmppyc/client): copy paste__init__.py to __init__.pyi

--- rusmppy/rusmppyc/python/rusmppyc/__init__.pyi
+++ rusmppy/rusmppyc/python/rusmppyc/__init__.pyi
@@ -4,3 +4,5 @@ from .rusmppyc import *  # type: ignore  # low-level Rust bindings  # noqa: F403
 from .client import Client as Client
 from .events import Events as Events
 from . import exceptions as exceptions
+
+__submodules__ = ["exceptions"]

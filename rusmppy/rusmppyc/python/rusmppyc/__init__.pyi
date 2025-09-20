1e674a70bd28dfce999937cacf3c8535cf61ebb0
fab <fabian.bernhart@gselectronic.com>
2025-09-20 18:45:23 +0200

    chore(rusmppyc): remove unused __submodules__ declaration

--- rusmppy/rusmppyc/python/rusmppyc/__init__.pyi
+++ rusmppy/rusmppyc/python/rusmppyc/__init__.pyi
@@ -4,5 +4,3 @@ from .rusmppyc import *  # type: ignore  # low-level Rust bindings  # noqa: F403
 from .client import Client as Client
 from .events import Events as Events
 from . import exceptions as exceptions
-
-__submodules__ = ["exceptions"]

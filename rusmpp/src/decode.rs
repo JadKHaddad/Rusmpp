d9caa14c82c9e727642f47993cd4226f5c438e67
Jad K. Haddad <jadkhaddad@gmail.com>
2025-10-26 11:31:41 +0100

    feat(rusmpp): added DecodeErrorSource when verbose feature is enabled
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- rusmpp/src/decode.rs
+++ rusmpp/src/decode.rs
@@ -3,3 +3,7 @@
 pub use rusmpp_core::decode::{
     COctetStringDecodeError, DecodeError, DecodeErrorKind, OctetStringDecodeError, owned::*,
 };
+
+#[cfg(feature = "verbose")]
+#[cfg_attr(docsrs, doc(cfg(feature = "verbose")))]
+pub use rusmpp_core::decode::DecodeErrorSource;

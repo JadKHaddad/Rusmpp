b283bd32986ec47070379e3b9a09dbec12bfe999
Jad K. Haddad <jadkhaddad@gmail.com>
2025-11-14 19:47:53 +0100

    dev(rusmppyc): print build env
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- rusmppy/rusmppyc/build.rs
+++ rusmppy/rusmppyc/build.rs
@@ -4,18 +4,21 @@ fn main() {
 
     // set by maturin
     if let Ok(sig) = std::env::var("PYO3_ENVIRONMENT_SIGNATURE") {
-        println!("cargo:info=PYO3_ENVIRONMENT_SIGNATURE = {sig}");
+        println!("cargo:warning=PYO3_ENVIRONMENT_SIGNATURE = {sig}");
 
         let sig_lower = sig.to_lowercase();
 
         if sig_lower.starts_with("pypy") {
-            println!("cargo:info=Detected PyPy environment");
+            println!("cargo:warning=Detected PyPy environment");
             println!("cargo:rustc-cfg=PyO3_PyPy");
         }
 
         if sig_lower.starts_with("graalpy") {
-            println!("cargo:info=Detected GraalPy environment");
+            println!("cargo:warning=Detected GraalPy environment");
             println!("cargo:rustc-cfg=PyO3_GraalPy");
         }
     }
+
+    println!("cargo:warning=PYO3_ENVIRONMENT_SIGNATURE = NOT SET");
+    println!("cargo:rerun-if-env-changed=PYO3_ENVIRONMENT_SIGNATURE");
 }

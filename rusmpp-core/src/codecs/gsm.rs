3d1ac7df317d4f785ddc05464ab2001c7ae85931
Jad K. Haddad <jadkhaddad@gmail.com>
2025-11-22 09:05:31 +0100

    feat(rusmpp-core/codecs): added a test for Gsm7UnpackedCodec
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- rusmpp-core/src/codecs/gsm.rs
+++ rusmpp-core/src/codecs/gsm.rs
@@ -258,3 +258,58 @@ mod lookup {
         'à' => 0x7F,
     };
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::codecs::owned::Encoder;
+
+    use super::*;
+
+    #[test]
+    fn encode() {
+        // c-spell: disable
+        let input = r##"Hello world!
+
+@£$¥èéùìòÇØøÅåΔ_ΦΓΛΩΠΨΣΘΞÆæßÉ !"#¤%&'()*+,-./0123456789:;<=>?¡ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ§¿abcdefghijklmnopqrstuvwxyzäöñüà
+
+^{}\[~]|€"##;
+        // c-spell: enable
+
+        let encoded = Gsm7UnpackedCodec::new()
+            .encode(input.as_bytes())
+            .expect("Encoding failed");
+
+        #[rustfmt::skip]
+        let expected: &[u8] = &[
+            // "Hello world!\n\n"
+            b'H', b'e', b'l', b'l', b'o', b' ', b'w', b'o', b'r', b'l', b'd', b'!', 0x0a, 0x0a,
+            // 00–09, 0b–0c, 0e–0f
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0b, 0x0c, 0x0e, 0x0f,
+            // 10–1f
+            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1c, 0x1d, 0x1e,
+            0x1f,
+            // ASCII printable range 0x20–0x7f
+            // !"#$%&'()*+,-./0123456789:;<=>?@
+            b' ', b'!', b'"', b'#', b'$', b'%', b'&', b'\'', b'(', b')', b'*', b'+', b',', b'-',
+            b'.', b'/', b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b':', b';',
+            b'<', b'=', b'>', b'?', b'@', 
+            // A–Z
+            b'A', b'B', b'C', b'D', b'E', b'F', b'G', b'H', b'I', b'J', b'K', b'L', b'M', b'N',
+            b'O', b'P', b'Q', b'R', b'S', b'T', b'U', b'V', b'W', b'X', b'Y', b'Z',
+            // [\]^_`
+            b'[', b'\\', b']', b'^', b'_', b'`', 
+            // a–z
+            b'a', b'b', b'c', b'd', b'e', b'f', b'g', b'h', b'i', b'j', b'k', b'l', b'm', b'n',
+            b'o', b'p', b'q', b'r', b's', b't', b'u', b'v', b'w', b'x', b'y', b'z',
+            // {|}~ 0x7f
+            b'{', b'|', b'}', b'~', 0x7f, 
+            // 0a 0a
+            0x0a, 0x0a, 
+            // 1b 14 1b ( 1b ) 1b / 1b < 1b = 1b > 1b @ 1b e
+            0x1b, 0x14, 0x1b, b'(', 0x1b, b')', 0x1b, b'/', 0x1b, b'<', 0x1b, b'=', 0x1b, b'>',
+            0x1b, b'@', 0x1b, b'e',
+        ];
+
+        assert_eq!(encoded.as_slice(), expected);
+    }
+}
